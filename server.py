e39b421e0c246e8c05557ccde8b65aab4a4c8548
Nick Avgerinos <taro@axcella.com>
2013-10-18 11:14:35 -0700

    Listen until client disconnects

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 import socket
 
+PORT = 8089
+
 # Create a Server Socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
+serversocket.bind(('localhost', PORT))
 serversocket.listen(5)	# Open socket for listening, max of 5 connections
 
 while True:
+	print "Server listening on port: ", PORT
 	connection, address = serversocket.accept()
-	buf = connection.recv(64)
+	
+	running = True
+	while running:
+		buf = connection.recv(64)
 
-	if len(buf) > 0:
-		print buf
-		break
+		if len(buf) > 0:
+			print buf
+	#		break
+		else:
+			running = False
+
+
 
