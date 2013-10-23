b23f42f3c6bba9a70d4f64ea3ee18ce451bd1b36
Nick Avgerinos <nicka@axcella.com>
2013-10-22 23:09:38 -0700

    Changing Port

--- chatserver.py
+++ chatserver.py
@@ -5,7 +5,7 @@ import sys
 class Server:
     def __init__(self):
         self.host    = ''
-        self.port    = 5000
+        self.port    = 5555
         self.server  = None
         self.inputs  = []
         self.running = True
